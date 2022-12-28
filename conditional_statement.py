88925528487adbe6dbce16e2a976f45c1ef35c12
Smikey <smikeywork@gmail.com>
2022-12-28 17:27:14 +0545

    nested if else

--- conditional_statement.py
+++ conditional_statement.py
@@ -7,6 +7,19 @@ if age>20:
 else:
     print("you cannot watch movie \n your age is less than 20 years")
 
+
+if age<0:
+    print("Age Invalid!!")
+elif age<5:
+    print("Ticket free")
+elif 10<age<30:
+    print("your ticket price is: 200")
+elif 31<age<55:
+    print("your ticket price is: 400")
+else:
+    pass
+
+
 fname = input("Please enter your name: ")
 
 if "a" in fname:
